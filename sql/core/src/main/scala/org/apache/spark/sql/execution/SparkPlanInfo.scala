77daf09504deee9840c34ca61ffcce28c68b7b34
Ryan Blue <blue@apache.org>
2017-09-25 15:16:25 -0700

    NETFLIX-BUILD: Add details back to SparkPlanInfo.
    This is needed for history server compatibility.
    
    (cherry picked from commit 8c7e6180ad9a6cd67b1e4f9ab7892604045a7475)

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlanInfo.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlanInfo.scala
@@ -29,9 +29,19 @@ import org.apache.spark.sql.execution.metric.SQLMetricInfo
 class SparkPlanInfo(
     val nodeName: String,
     val simpleString: String,
+    val details: Map[String, Any],
     val children: Seq[SparkPlanInfo],
     val metadata: Map[String, String],
     val metrics: Seq[SQLMetricInfo]) {
+
+  def this(
+      nodeName: String,
+      simpleString: String,
+      children: Seq[SparkPlanInfo],
+      metadata: Map[String, String],
+      metrics: Seq[SQLMetricInfo]) = {
+    this(nodeName, simpleString, Map.empty, children, metadata, metrics)
+  }
 
   override def hashCode(): Int = {
     // hashCode of simpleString should be good enough to distinguish the plans from each other
