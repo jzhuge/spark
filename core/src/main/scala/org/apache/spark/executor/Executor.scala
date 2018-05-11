7d52448d5673f0cbcdb21e9b36d15e317e25c23e
Ashwin Shankar <ashankar@netflix.com>
2018-05-11 14:21:32 -0700

    NETFLIX-BUILD: Report executor's actual memory usage
    (cherry picked from commit a2016127963ee17b3dc3a013a642e39d5b91dd4a)
    
    Conflicts:
    	core/src/main/scala/org/apache/spark/executor/Executor.scala
    
    Fix scala style errors in Executor.scala
    
    (cherry picked from commit a8349182c1a305a8c8de670e0578b11dae565b4d)
    
    Conflicts:
    	core/src/main/scala/org/apache/spark/executor/Executor.scala

--- core/src/main/scala/org/apache/spark/executor/Executor.scala
+++ core/src/main/scala/org/apache/spark/executor/Executor.scala
@@ -30,6 +30,7 @@ import scala.collection.JavaConverters._
 import scala.collection.mutable.{ArrayBuffer, HashMap, Map}
 import scala.util.control.NonFatal
 
+import com.codahale.metrics.Histogram
 import com.google.common.util.concurrent.ThreadFactoryBuilder
 import com.netflix.bdp
 
@@ -169,6 +170,27 @@ private[spark] class Executor(
   private var heartbeatFailures = 0
 
   startDriverHeartbeater()
+
+  private val memoryReporter = ThreadUtils.newDaemonSingleThreadScheduledExecutor("memory-reporter")
+
+  private val usedMemory: Histogram =
+    com.netflix.bdp.TaskMetrics.histogram("executor-memory-usage", "usedMemory")
+
+  if (conf.getBoolean("spark.executor.memory-reporter.enable", false)) {
+    startMemoryReporter()
+  }
+
+  private def startMemoryReporter(): Unit = {
+    val memoryReporterTask = new Runnable() {
+      override def run(): Unit = {
+        System.gc()
+        usedMemory.update(Runtime.getRuntime.totalMemory() - Runtime.getRuntime.freeMemory())
+      }
+    }
+    log.info("Starting memoryReporter")
+    memoryReporter.scheduleAtFixedRate(memoryReporterTask,
+      1000, conf.getInt("spark.executor.memory-reporter.interval", 10000), TimeUnit.MILLISECONDS)
+  }
 
   private[executor] def numRunningTasks: Int = runningTasks.size()
 
@@ -818,6 +840,8 @@ private[spark] class Executor(
     heartbeater.scheduleAtFixedRate(heartbeatTask, initialDelay, intervalMs, TimeUnit.MILLISECONDS)
   }
 }
+
+
 
 private[spark] object Executor {
   // This is reserved for internal use by components that need to read task properties before a
