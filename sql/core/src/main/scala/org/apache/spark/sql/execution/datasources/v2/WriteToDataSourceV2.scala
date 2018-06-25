f05e0e858150fc6b4b14c439849f2c8703357f8f
Marcelo Vanzin <vanzin@cloudera.com>
2018-06-25 16:55:41 -0700

    [SPARK-24552][CORE][SQL][BRANCH-2.3] Use unique id instead of attempt number for writes .
    This passes a unique attempt id instead of attempt number to v2
    data sources and hadoop APIs, because attempt number is reused
    when stages are retried. When attempt numbers are reused, sources
    that track data by partition id and attempt number may incorrectly
    clean up data because the same attempt number can be both committed
    and aborted.
    
    Author: Marcelo Vanzin <vanzin@cloudera.com>
    
    Closes #21615 from vanzin/SPARK-24552-2.3.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/WriteToDataSourceV2.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/WriteToDataSourceV2.scala
@@ -98,8 +98,8 @@ object DataWritingSparkTask extends Logging {
       useCommitCoordinator: Boolean): WriterCommitMessage = {
     val stageId = context.stageId()
     val partId = context.partitionId()
-    val attemptId = context.attemptNumber()
-    val dataWriter = writeTask.createDataWriter(partId, attemptId)
+    val attemptId = context.taskAttemptId().toInt // see SPARK-24552
+    val dataWriter = writeTask.createDataWriter(context.partitionId(), attemptId)
 
     // write the data and commit this writer.
     Utils.tryWithSafeFinallyAndFailureCallbacks(block = {
