2800ba692b763128bebd0eeb4a3873ff7800eb39
Ryan Blue <blue@apache.org>
2018-09-04 15:53:37 -0700

    NETFLIX-BUILD: Fix ScalaStyle problesm in SparkSession.

--- sql/core/src/main/scala/org/apache/spark/sql/SparkSession.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/SparkSession.scala
@@ -42,11 +42,11 @@ import org.apache.spark.sql.catalyst.plans.logical.{LocalRelation, Range}
 import org.apache.spark.sql.execution._
 import org.apache.spark.sql.execution.datasources.LogicalRelation
 import org.apache.spark.sql.execution.ui.SQLListener
-import org.apache.spark.sql.internal.{CatalogImpl, SQLConf, SessionState, SharedState}
+import org.apache.spark.sql.internal.{CatalogImpl, SessionState, SharedState, SQLConf}
 import org.apache.spark.sql.internal.StaticSQLConf.CATALOG_IMPLEMENTATION
 import org.apache.spark.sql.sources.BaseRelation
 import org.apache.spark.sql.streaming._
-import org.apache.spark.sql.types.{DataType, LongType, StructType}
+import org.apache.spark.sql.types.{DataType, StructType}
 import org.apache.spark.sql.util.ExecutionListenerManager
 import org.apache.spark.util.Utils
 
