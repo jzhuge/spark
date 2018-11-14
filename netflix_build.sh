96bdee6e75a1f779ea931603a754b5d31cd759d6
Ryan Blue <blue@apache.org>
2018-11-14 14:14:55 -0800

    NETFLIX-BUILD: Use Jenkins role to fix integration test failures.

--- netflix_build.sh
+++ netflix_build.sh
@@ -115,31 +115,10 @@ fi
 export HADOOP_CONF_DIR=${WORKSPACE}/netflix/test-conf
 export HADOOP_HOME=${WORKSPACE}/tmp/hadoop
 
-export BUILD_CREDENTIALS=$( \
-  aws sts assume-role \
-    --role-arn arn:aws:iam::219382154434:role/DATAOVEN_S3_ROLE \
-    --role-session-name $USER \
-  | jq '.Credentials')
-
-export AWS_ACCESS_KEY_ID=$(jq -r '.AccessKeyId' <<<$BUILD_CREDENTIALS)
-export AWS_SECRET_ACCESS_KEY=$(jq -r '.SecretAccessKey' <<<$BUILD_CREDENTIALS)
-export AWS_SESSION_TOKEN=$(jq -r '.SessionToken' <<<$BUILD_CREDENTIALS)
-
 if ${WORKSPACE}/spark-${BUILD_VERSION}/bin/spark-submit netflix/integration_tests.py; then
   echo
   echo 'Integration tests PASSED. Deploying tarball to app location.'
   echo
-
-  export BUILD_CREDENTIALS=$( \
-    aws sts assume-role \
-      --role-arn arn:aws:iam::219382154434:role/BDP_JENKINS_ROLE \
-      --role-session-name $USER \
-    | jq '.Credentials')
-
-  export AWS_ACCESS_KEY_ID=$(jq -r '.AccessKeyId' <<<$BUILD_CREDENTIALS)
-  export AWS_SECRET_ACCESS_KEY=$(jq -r '.SecretAccessKey' <<<$BUILD_CREDENTIALS)
-  export AWS_SESSION_TOKEN=$(jq -r '.SessionToken' <<<$BUILD_CREDENTIALS)
-
   aws s3 cp spark-${BUILD_VERSION}.tgz s3://netflix-bigdataplatform/spark-builds/${BUILD_VERSION}/
 else
   echo
