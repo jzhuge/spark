2407048a50c2c29ccec8069b8575e9d5f1d24fd1
Ryan Blue <blue@apache.org>
2019-05-22 10:56:18 -0700

    NETFLIX-BUILD: Update AST builder allow multipart function names.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -1421,7 +1421,8 @@ class Analyzer(
             } catch {
               case unsupported: UnsupportedOperationException =>
                 failAnalysis(s"Function ${unbound.name} cannot process input: " +
-                    s"${children.map(_.dataType.simpleString)}: ${unsupported.getMessage}")
+                    s"(${children.map(_.dataType.simpleString).mkString(", ")}): " +
+                    unsupported.getMessage)
             }
 
             bound match {
