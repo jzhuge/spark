12a66083abea334e4a2c141b15dfe479c7551e82
John Zhuge <jzhuge@apache.org>
2019-06-30 19:37:20 -0700

    NETFLIX-BUILD: Bump bdp-metrics to 0.3.5-rc.5

--- core/src/main/scala/org/apache/spark/executor/Executor.scala
+++ core/src/main/scala/org/apache/spark/executor/Executor.scala
@@ -32,6 +32,7 @@ import scala.util.control.NonFatal
 import com.codahale.metrics.Histogram
 import com.netflix.bdp
 import com.netflix.bdp.GarbageCollectionMetrics
+import com.netflix.bdp.TaskMetrics.addTag
 
 import org.apache.spark._
 import org.apache.spark.deploy.SparkHadoopUtil
@@ -156,8 +157,8 @@ private[spark] class Executor(
   startDriverHeartbeater()
 
   if (!isLocal && conf.getBoolean("spark.report.gc.metrics", true)) {
-    System.setProperty("spark.app.id", conf.getAppId)
-    conf.getOption("spark.genie.id").foreach(System.setProperty("spark.genie.id", _))
+    addTag("app", conf.getAppId)
+    conf.getOption("spark.genie.id").foreach(addTag("job", _))
     GarbageCollectionMetrics.registerListener()
   }
 
