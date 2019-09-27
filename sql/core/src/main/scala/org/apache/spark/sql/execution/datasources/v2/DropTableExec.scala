1e6a27ac90ff93fbb43573a7e0fa521acd409cde
Ryan Blue <blue@apache.org>
2019-09-27 13:59:57 -0700

    NETFLIX-BUILD: Refresh v2 tables after drop.
    This includes the following squashed commits:
    NETFLIX-BUILD: Refresh Spark's built-in catalog when tables are dropped.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DropTableExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DropTableExec.scala
@@ -36,6 +36,8 @@ case class DropTableExec(
       throw new NoSuchTableException(identifier.database.get, identifier.table)
     }
 
+    sqlContext.sessionState.catalog.refreshTable(identifier)
+
     sqlContext.sparkContext.parallelize(Seq.empty, 1)
   }
 
